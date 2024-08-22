dd06e727dfb4dfeadc5ada6030ab2a2bcd32c72f
Mahedee Hasan <mahedee007@gmail.com>
2024-08-23 03:09:03 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    fontFamily:{
+      'manrope': ['Manrope', 'sans-serif']
+  },
+
+    extend: {
+      colors: {
+        'btn-primary': '#ABEF5F',
+        'text-primary': '#212121',
+        'text-secondary': '#494949',
+        'text-third': '#737373',
+        'text-forth': '#1C1C1C',
+        'bg-primary': '#F4F4F4'
+        
+       
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
